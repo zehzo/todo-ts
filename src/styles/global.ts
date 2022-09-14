e5a54ab50217b4dc2f25cc38069ebaf1331486a7
zehzo <edson.ferreiraj@hotmail.com>
2022-09-14 00:58:54 -0300

    implementando funcao de deletar task

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding:0;
   box-sizing: border-box;
-  
+
   }
 
   body {
@@ -16,6 +16,8 @@ export const GlobalStyle = createGlobalStyle`
   
     font-family: 'Inter', sans-serif;
     font-size: 1rem;
-    padding: 10vh 0vh;
+    /* padding: 10vh 0vh; */
+    overflow: hidden;
+    
   } 
 `
