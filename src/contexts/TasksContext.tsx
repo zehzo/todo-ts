60d3b72f38486a63042835ce7293095939e086d4
zehzo <edson.ferreiraj@hotmail.com>
2022-09-16 20:47:39 -0300

    refatorando codigo

--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  ReactNode,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { createTask, deleteTask, listTasks } from '../api/tasks';
 
 interface Task {
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,33 +1,31 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const HomeContainer = styled.main`
   border-radius: 10px;
-  background-color: ${props => props.theme["gray-600"]};
+  background-color: ${(props) => props.theme['gray-600']};
   display: flex;
   flex-direction: column;
-  @media (max-width: 768px){
-    /* margin: 1vh; */
+  @media (max-width: 768px) {
   }
-`
+`;
 
 export const WrapperContainer = styled.div`
   display: grid;
   grid-template-columns: 300px 1fr;
-  color: ${props => props.theme.white};
-  @media (max-width: 768px){
+  color: ${(props) => props.theme.white};
+  @media (max-width: 768px) {
     grid-template-columns: 1fr;
   }
-`
+`;
 
 export const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
-  background-color: ${props => props.theme["gray-700"]};
+  background-color: ${(props) => props.theme['gray-700']};
   padding: 30px;
   border-radius: 0 0 4px 0;
-  @media (max-width: 768px){
-    background-color: ${props => props.theme["gray-700"]};
+  @media (max-width: 768px) {
+    background-color: ${(props) => props.theme['gray-700']};
     height: 100vh;
   }
-`
-
+`;
