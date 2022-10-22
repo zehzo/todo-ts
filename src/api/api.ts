c58239d3d4b8d657a747cd8a6edb9f4463b28418
zehzo <edson.ferreiraj@hotmail.com>
2022-10-22 15:03:15 -0300

    corrigindo deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.API || 'https://jose-todo-list-api.herokuapp.com/',
 });
 
 export default api;
