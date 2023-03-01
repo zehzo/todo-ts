5774c01062e3c314354434be38086f8b270cfd8c
zehzo <edson.ferreiraj@hotmail.com>
2023-03-01 13:54:00 -0300

    corrigindo app para rodar localmente junto com a api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API || 'https://jose-todo-list-api.herokuapp.com/',
+  baseURL: 'http://localhost:3000',
 });
 
 export default api;
